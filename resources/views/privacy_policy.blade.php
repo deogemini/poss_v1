11740b8a77cc811cbc9f124d2e8bfc3f46bf0854
Deo Gemini <geminideogratias8@gmail.com>
2022-06-28 10:35:18 +0300

    Update privacy_policy.blade.php

--- resources/views/privacy_policy.blade.php
+++ resources/views/privacy_policy.blade.php
@@ -17,15 +17,67 @@
 
 
 <div class="container">
-<H1>PRIVACY POLICY</H1>
-<p>This Privacy Statement explains our practices, including your choices, regarding the collection, use, and disclosure of certain information, including your personal information in connection with the POSS APP.</p>
-<H2>Contacting Us</H2>
-<p>If you have general questions about how the POSS APP works, please visit our online help center at <a href="http://178.62.199.223/">helpdesk@poss.app</a> </p>
+<h1>PRIVACY POLICY</h1>
+<p>
+This Privacy policy explains our practices, including regarding your  use, and disclosure of certain information, including your information in connection with the Poss application.Occasionally Poss is implemented into an environment where it is integrated with a Student Information System or Learning Management System . These integrations are to monitor attendance of secondary students by the teachers and may require your personal information to ensure accounts in each system are synchronized
+</p>
+<h2>Security</h2>
+<p>We use reasonable administrative, logical, physical and managerial measures to safeguard your personal information against loss, theft and unauthorized access, use and modification. These measures are designed to provide a level of security appropriate to the risks of processing your personal information.</p>
 
+<br><br>
+<p>In Poss your right to privacy is very important to us. We provide you with the ability to create, share, edit, access, manage, print, provide feedback on students attendance. We collect certain specific information from you, and we also automatically collect information and data relating to the usage of our services by all our users, including you which provides us with aggregate data that Poss uses to improve the Services. Because we are committed to protecting your privacy rights and the information you provide to us, we have developed this privacy policy. It explains how we will protect your information and describes how we will use and share it.</p>
+<br>
+<p>This policy is concerned with the protection of your personal information only. Personal information includes any data that can be used to identify you as an individual, such as your name, e-mail address.</p>
+<br>
+<p>Poss may use aggregated personal information to determine the demographic composition of our user base.</p>
+<br>
+<p>This policy describes the following practices:</p>
+<ul>
+    <li><p>Collecting your personal information</p></li>
+    <li><p>Use of your personal information</p></li>
+    <li><p>Sharing your personal information</p></li>
+    <li><p>Maintaining your personal information</p></li>
+</ul>
+<p>
+This privacy policy may change from time to time to reflect Poss nternal needs, customer feedback, or changes in applicable privacy legislation. Poss will post all such changes on our website, so please visit this page regularly.
+</p>
+<br>
+<h2>Sharing your personal information</h2>
+<p>In sharing your information we will follow these practices regarding the distribution of that information.</p>
+<h2>Your personal information is always protected</h2>
+<p>Poss uses reasonable precautions to protect your personal information and store it securely. Access to your personal information is restricted to those personnel who need it, and Poss takes reasonable steps to prevent the unauthorized use or disclosure of your personal information.</p>
+<p>Poss only uses and discloses your personal information if it helps us facilitate your needs, improve our products and services, meet legal and regulatory requirements, or — to the limited extent possible — manage our internal business operations. Additionally, if there are any disclosure or use restrictions in your consent to the collection of your personal information, Poss will abide by those restriction.</p>
+<br>
+<h2>We only share your information with your consent.</h2>
+<p>Poss provides your information to third parties only for purposes to which you have consented, and we require such third parties to keep your information confidential.</p>
+<p>In a few situations, Poss may be required to disclose your information without your prior consent. For example,Poss may disclose your information if we are required to do so by law, or if we believe in good faith that such disclosure is necessary to:</p>
+<ul>
+    <li><p>comply with law or legal process</li>
+    <li>protect and defend our rights and property, or the rights and property of a third party</li>
+    <li>protect against misuse or unauthorized use of any Poss products, software, services, or other proprietary materials</li>
+    <li>protect the personal safety of any person</li>
+    <li>allow for a change of ownership of Poss and associated transfer of all personal information to the new owner of Poss – this does not affect the protection of your information under this Privacy Policy</li>
+    <li>Poss will always try to provide you with prior notice of such disclosure; however, such notice may not always be possible or reasonable given the circumstances.
+    </li>
+</ul>
+<p>Additionally, the nature of the Services provided requires the routine use of your information in the provision of the Services. Your personal information will only be used to the limited extent necessary to provide the Services that you have requested.</p>
+<br>
+<br>
+<h2>Contacting us</h2>
+<p>If you have general questions about your account or how to contact customer service for assistance, please visit our online help center at <a href="">helpcenter@poss.com</a> For questions specifically about this Privacy Statement, or our use of your personal information, cookies or similar technologies, please contact our Data Protection Officer/Privacy Office by email at <a href="">info@poss.com</a> </p>
 </div>
 
+<footer class="page-footer font-small cyan">
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2022 Copyright:
+  <a href="/"> poss.com</a>
+</div>
+<!-- Copyright -->
+
+</footer>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
-    
+
 </body>
 </html>

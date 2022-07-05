4a04876bf2e3bb2af024a358265b52d0e5924c12
Deo Gemini <geminideogratias8@gmail.com>
2022-07-05 20:11:05 +0300

    Feature: index for regions pulling data from backend

--- resources/views/dashboard/region/index.blade.php
+++ resources/views/dashboard/region/index.blade.php
@@ -3,27 +3,76 @@
 @section('content')
 
 <div class="row">
-    <div class="col-md-8" style="margin-bottom: 10px ;">
+  <div class="col-md-8" style="margin-bottom: 10px ;">
     <a href="javascript::void()" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#modal-add-role">Add Region</a>
-    </div>
+  </div>
 </div>
 <div class="row">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-body">
-                
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-body">
 
-            
+        <table class="table table-striped data-table table-bordered table-hover display" id="example" style="width:100%">
+          <thead>
+            <tr>
+              <th scope="col">C/N</th>
+              <th scope="col">Name</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
 
-            </div>
-        </div>
+            @foreach($regions as $region)
+            <tr>
+              <td> {{ $loop-> index + 1 }}</td>
+              <td>{{ $region->name }}</td>
+              <td>
+                <a href="javascript::void()" class="btn btn-xs btn-primary" data-toggle="modal" data-target="#modal-edit-role-{{$region->id}}">Edit</a>
+                <a href="javascript::void()" class="btn btn-danger btn-xs" onclick="if(confirm('Are you sure you want to delete this role ?')){
+                              	getElementById('delete-role-{{$region->id}}').submit()}">Delete</a>
+                <form action="/roles/{{$region->id}}" method="post" style="display: inline-block;" id="delete-role-{{$region->id}}">
+                  @csrf
+                  @method('DELETE')
+                </form>
+
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Actions</th>
+            </tr>
+          </tfoot>
+        </table>
 
 
+
+
+      </div>
     </div>
-   
+
+
+  </div>
+
 
 
 </div>
+
+
+
+<script>
+  $(document).ready(function() {
+    $('#example').DataTable({
+      paging: false,
+      ordering: false,
+      info: false,
+    });
+  });
+</script>
+
 
 
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,8 +16,9 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css"> 
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+  
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
@@ -335,6 +336,7 @@ a.article:hover {
 
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<!-- <script src="cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js" ></script> -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script type="text/javascript">

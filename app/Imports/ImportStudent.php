2be0019248a2360f2a9d106aedace038ec4da1ae
Deogratias Gemini <geminideogratias8@gmail.com>
2023-02-17 19:03:52 +0300

    change

--- app/Imports/ImportStudent.php
+++ app/Imports/ImportStudent.php
@@ -26,14 +26,14 @@ class ImportStudent implements ToModel, WithHeadingRow
         $this->school_id = $school_id;
     }
 
-    public function model(array $rows)
+    public function model(array $row)
     {
         // Look up the stream_id based on the stream name
-        $stream = Stream::where('name', $rows['Stream'])->first();
+        $stream = Stream::where('name', $row['Stream'])->first();
         $stream_id = $stream ? $stream->id : null;
 
         // Convert gender value to lowercase
-        $gender = strtolower($rows['Gender']);
+        $gender = strtolower($row['Gender']);
 
 
         // Calculate the final year based on the provided grade class and school's education level
@@ -51,7 +51,7 @@ class ImportStudent implements ToModel, WithHeadingRow
                     'Standard Seven' => 7,
                 ];
 
-                $yearOffset = 7 - $nameToValue[$rows['Grade']];
+                $yearOffset = array_key_exists($row['Grade'], $nameToValue) ? 7 - $nameToValue[$row['Grade']] : 0;
                 break;
             case 'Secondary':
                 $nameToValue = [
@@ -60,7 +60,7 @@ class ImportStudent implements ToModel, WithHeadingRow
                     'Form Three' => 3,
                     'Form Four' => 4,
                 ];
-                $yearOffset = 4 - $nameToValue[$rows['Grade']];
+                $yearOffset = 4 - array_key_exists($row['Grade'], $nameToValue) ? 7 - $nameToValue[$row['Grade']] : 0;
                 break;
             default:
                 $yearOffset = 0;
@@ -74,7 +74,7 @@ class ImportStudent implements ToModel, WithHeadingRow
         $final_year_id = $final_year ? $final_year->id : null;
 
         $student =  new Student([
-            'student_name' => $rows['Name of Student'],
+            'student_name' => $row['Name of Student'],
             'gender' => $gender,
             'stream_id' => $stream_id,
             'school_id' => $this->school_id,

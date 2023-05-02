84ec78cc15df0160fa6ed85401701ac3c1c86e97
Deogratias Gemini <geminideogratias8@gmail.com>
2023-05-02 14:43:12 +0300

    Initialize the chart js for the graphs

--- resources/views/dashboard/attendanceReports/index.blade.php
+++ resources/views/dashboard/attendanceReports/index.blade.php
@@ -70,6 +70,43 @@
             </div>
         </div>
 
+        <div class="container">
+            <canvas id="myChart"></canvas>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+        <script>
+            const ctx = document.getElementById('myChart');
+
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['Present', 'Absent', 'WithPermit', 'No Permit'],
+                    datasets: [{
+                        label: '# No of Students',
+                        data: [12, 19, 3, 5],
+                        borderWidth: 1,
+                        backgroundColor: [
+                            'rgba(255, 99, 132)',
+                            'rgba(255, 159, 64)',
+                            'rgba(255, 205, 8)',
+                            'rgba(75, 192, 192)'
+                        ]
+                    },
+                    ] ,
+                },
+                options: {
+                    indexAxis: 'y',
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        </script>
+
 
 
     <script type="text/javascript">
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -27,9 +27,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" />
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" /> -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-    <!-- <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
- -->
+
 
 
     <!-- Styles -->
@@ -481,7 +479,6 @@ $role = $role_name->name;
 
 
 
-<!-- <script src="cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js" ></script> -->
 <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
